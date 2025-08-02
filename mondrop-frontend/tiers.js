75544981cf86d7dd1f7f3dc35216306b063db348
Tiger121612 <159625046+Tiger121612@users.noreply.github.com>
2025-08-02 11:06:11 +0530

    Create tiers.js

---
+++ mondrop-frontend/tiers.js
@@ -0,0 +1,10 @@
+export const tierImages = {
+  OG: "https://i.imgur.com/Z7ZQ6eE.png",
+  Diamond: "https://i.imgur.com/UbYbhdo.png",
+  Gold: "https://i.imgur.com/BV6q0gN.png",
+  Silver: "https://i.imgur.com/pOW23cd.png",
+  Pro: "https://i.imgur.com/zKgf5Ik.png",
+  Builder: "https://i.imgur.com/RydKf4o.png",
+  Bronze: "https://i.imgur.com/Ob1n7il.png",
+  NotEligible: "https://i.imgur.com/LdFVwGR.png",
+};
